d6465b20314159f278040940bbbe9f47455c13a2
Anderson Lourenço <111094203+andersonlourenc@users.noreply.github.com>
2022-09-25 12:16:01 -0300

    marioless

---
+++ Lab. de Programação/Mario Less/marioless.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <cs50.h>
+
+int main () {
+    
+    int altura;
+    do {
+    altura = get_int ("Entre com a altura: ");
+    }
+    while (altura < 1 || altura > 8);
+    
+    for (int linha = 0; linha < altura; linha++) {
+        for (int space = 0; space < altura - linha - 1; space++){
+            printf(" ");
+        }
+        for(int coluna =0; coluna <= linha; coluna++) {
+        printf("#");
+        }
+        printf("\n");
+    }
+}
