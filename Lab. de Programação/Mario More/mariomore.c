ee856b2f6204792cbd46999b38d68621f46bb9ab
Anderson Lourenço <111094203+andersonlourenc@users.noreply.github.com>
2022-11-26 15:16:51 -0300

    mario more

--- Lab. de Programação/Mario More/mariomore.c
+++ Lab. de Programação/Mario More/mariomore.c
@@ -1,3 +1,8 @@
+/*
+Aluno: Anderson Souza Lourenço
+Matrícula: 20212045050347
+https://github.com/andersonlourenc/Faculdade/blob/b082eb6e866264a52cc17160e626055f98e73df0/Lab.%20de%20Programa%C3%A7%C3%A3o/Mario%20More/mariomore.c
+
 #include <stdio.h>
 #include <cs50.h>
 
